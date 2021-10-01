9afac089e496a76234e06e444709b3dd6c900ebe
pay <91664022+paypay29@users.noreply.github.com>
2021-10-01 22:29:51 +0900

    Add files via upload

--- line_test.py
+++ line_test.py
@@ -9,6 +9,9 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, ImageSendMessage,
 )
 import os
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import boto3
 
 app = Flask(__name__)
 
@@ -18,13 +21,14 @@ YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+aws_s3_bucket        = os.environ['AWS_BUCKET']
+
 
 user_id = "Ub58032ed8192de02a6209c8021fa535e"
 
-def sum(a):
-    ans = a + 5
-    
-    return(ans)
+def grh():
+    fig = plt.figure("帰宅時間")
+    ax = fig.add_subplot(1, 1, 1)
 
 #herokuへのデプロイが成功したかどうかを確認するためのコード
 @app.route("/")
@@ -58,11 +62,29 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text="https://drive.google.com/file/d/1R75O4z86XKYgStka-2qTelsxGY8AFWFI/view?usp=sharing"))
         """
+        grh()
+        plt.show()
+        file_name = "帰宅時間.png"
+        plt.savefig(file_name)
+        # S3へグラフ画像をアップロードする
+        s3_resource = boto3.resource('s3')
+        s3_resource.Bucket(aws_s3_bucket).upload_file(file_name, file_name)
+        # S3へアップロードした画像の署名付きURLを取得する
+        s3_client = boto3.client('s3')
+        s3_image_url = s3_client.generate_presigned_url(
+        ClientMethod = 'get_object',
+        Params       = {'Bucket': aws_s3_bucket, 'Key': file_name},
+        ExpiresIn    = 10,
+        HttpMethod   = 'GET'
+    )
+        
         line_bot_api.reply_message(
             event.reply_token,
             ImageSendMessage(
-                original_content_url = "https://drive.google.com/file/d/1R75O4z86XKYgStka-2qTelsxGY8AFWFI/view?usp=sharing",
-                preview_image_url = "https://drive.google.com/file/d/1R75O4z86XKYgStka-2qTelsxGY8AFWFI/view?usp=sharing"))
+                original_content_url = s3_image_url,
+                preview_image_url = s3_image_url
+            )
+        )
         
     elif event.message.text == "グラフ表示":
         line_bot_api.reply_message(
