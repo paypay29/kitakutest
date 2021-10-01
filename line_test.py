da6416ac2fbc8a1f81f60ab4517eb21ba4060319
pay <91664022+paypay29@users.noreply.github.com>
2021-10-01 15:30:28 +0900

    Update line_test.py

--- line_test.py
+++ line_test.py
@@ -50,7 +50,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="は？"))
+        TextSendMessage(text=""))
 
 
 # ポート番号の設定
